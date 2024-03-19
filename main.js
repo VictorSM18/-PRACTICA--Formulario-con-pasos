eb0b802abd5292c8c3298203f77095383e461d6c
Victor Samuel <victoress18@gmail.com>
2024-03-18 22:39:05 -0400

    Hecha la carta 3, Hecho el paso a paso y la paginacion

---
+++ main.js
@@ -0,0 +1,52 @@
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
+const setName = document.querySelector(".name");
+const setEmail = document.querySelector(".email");
+const topicList = document.querySelector(".topic-list");
+const pagination = document.querySelector(".pagination");
+
+button1.addEventListener("click", () => {
+    if (userName.value && userEmail.value) {
+        setName.innerHTML = userName.value;
+        setEmail.innerHTML = userEmail.value;
+
+        const step2 = pagination.children[2];
+        step2.classList.add("step-active");
+
+        document.body.children[0].classList.add("no-display");
+        document.body.children[1].classList.remove("no-display");
+    }
+});
+
+button2.addEventListener("click", () => {
+    let topicResults = getCheckedArray();
+
+    for (let topic of topicResults) {
+        let itemList = document.createElement("li");
+        itemList.innerHTML = topic;
+        topicList.appendChild(itemList);
+    }
+
+    const step2 = pagination.children[3];
+    step2.classList.add("step-active");
+
+    document.body.children[1].classList.add("no-display");
+    document.body.children[2].classList.remove("no-display");
+});
+
+button3.addEventListener("click", () => {
+    alert("✅ Success");
+});
+
+function getCheckedArray() {
+    let checkedArray = [];
+    document.querySelectorAll('[type="checkbox"]').forEach((item) => {
+        if (item.checked === true) {
+            checkedArray.push(item.value);
+        }
+    });
+    return checkedArray;
+}
